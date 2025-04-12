dc795ce0647c216ff59acacc681b047776f2584d
ambatiweb <ambatiweb@gmail.com>
2025-04-12 15:41:33 +0530

    Initial commit

---
+++ src/com/vcb/leet/code/integer/PalindromeNumber.java
@@ -0,0 +1,27 @@
+package com.vcb.leet.code.integer;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121)); // true
+        System.out.println(isPalindrome(-121)); // false
+        System.out.println(isPalindrome(10)); // false
+        System.out.println(isPalindrome(12321)); // true
+    }
+
+    public static boolean isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedNumber = 0;
+        while (x > reversedNumber) {
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Check if the number is a palindrome
+        return x == reversedNumber || x == reversedNumber / 10;
+    }
+}
