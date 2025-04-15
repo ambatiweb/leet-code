2b275ef90284a0de5e9e0a36aa02c2464594d465
ambatiweb <ambatiweb@gmail.com>
2025-04-15 08:35:38 +0530

    Leetcode programme to plus 1

---
+++ src/com/vcb/leet/code/arrays/PlusOne.java
@@ -0,0 +1,39 @@
+package com.vcb.leet.code.arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 5};
+        int[] result = plusOne(digits);
+        for (int digit : result) {
+            System.out.print(digit + " "); // Output: 1 2 4
+        }
+    }
+
+    public static int[] plusOne(int[] digits) {
+        // intially set curry to 1
+        int curry = 1;
+        for (int i = digits.length - 1; i >= 0 && curry == 1; i--) {
+            int tmp = digits[i] + curry;
+            // if we have nums[i] + 1 >= 10
+            if (tmp >= 10) {
+                //curry = 1;
+                digits[i] = tmp % 10;
+            } else {
+                curry = 0;
+                digits[i] = tmp;
+            }
+        }
+        // if we have input like [9,9,9] then we will have curry = 1
+        // so in this case we have an or of length len(digits) + 1
+        // first digits is 1 and rest copy from digits to newDigits array
+        if (curry != 0) {
+            int[] newDigits = new int[digits.length+1];
+            newDigits[0] = 1;
+            for (int i = 1; i < newDigits.length; i++) {
+                newDigits[i] = digits[i-1];
+            }
+            return newDigits;
+        }
+        return digits;
+    }
+}
