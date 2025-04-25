167ea712fdd9f542bd925db0377d99be5c0348e3
ambatiweb <ambatiweb@gmail.com>
2025-04-25 11:03:15 +0530

    Leetcode merge sorted array.

---
+++ src/com/vcb/leet/code/arrays/MergeSortedArray.java
@@ -0,0 +1,34 @@
+package com.vcb.leet.code.arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // Last index of nums1
+        int j = n - 1; // Last index of nums2
+        int k = m + n - 1; // Last index of merged array
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
