e3d3e8667c18e1878cfa742a43144a3d8f66eb02
ambatiweb <ambatiweb@gmail.com>
2025-04-26 08:08:04 +0530

    Leetcode Binary Search.

--- src/com/vcb/leet/code/arrays/BinarySearch.java
+++ src/com/vcb/leet/code/arrays/BinarySearch.java
@@ -1,9 +1,17 @@
 package com.vcb.leet.code.arrays;
 
+/**
+ * Binary Search - Leetcode - 704
+ * Given a sorted (in ascending order) integer array nums of n elements ad a target value,
+ * write a function to search target in nums.
+ * If target exists, then return its index, otherwise return -1.
+ * Solve it by log(n) complexity
+ */
+
 public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
-        int target = 3;
+        int target = 4;
         int result = binarySearch(nums, target);
         System.out.println("Index of " + target + ": " + result); // Output: Index of 3: 2
     }
@@ -13,6 +21,7 @@ public class BinarySearch {
         int right = nums.length - 1;
 
         while (left <= right) {
+            // Calculate the mid index
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
