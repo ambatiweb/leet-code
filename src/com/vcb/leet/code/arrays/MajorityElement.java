4ee7f404f990df9eddce8c574a20262df16c8c05
ambatiweb <ambatiweb@gmail.com>
2025-04-30 15:54:26 +0530

    Leetcode, find majority element.

---
+++ src/com/vcb/leet/code/arrays/MajorityElement.java
@@ -0,0 +1,22 @@
+package com.vcb.leet.code.arrays;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums)); // Output: 2
+    }
+
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = -1;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
