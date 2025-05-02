bc49426f559af0cc0819463ca932811e8839960c
ambatiweb <ambatiweb@gmail.com>
2025-05-02 17:24:44 +0530

    Leetcode, Group anagrams.

---
+++ src/com/vcb/leet/code/arrays/GroupAnagrams.java
@@ -0,0 +1,27 @@
+package com.vcb.leet.code.arrays;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
