4140989e6ebc53539f14f65b0c32eb20da112cb7
ambatiweb <ambatiweb@gmail.com>
2025-05-06 21:08:37 +0530

    Leetcode, Write a program for pascal's triangle.

---
+++ src/com/vcb/leet/code/arrays/PascalsTriangle.java
@@ -0,0 +1,30 @@
+package com.vcb.leet.code.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        int numRows = 5;
+        System.out.println(generate(numRows));
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int row = 0; row < numRows; row++) {
+            List<Integer> currentRow = new ArrayList<>();
+            for (int col = 0; col <= row; col++) {
+                if (col == 0 || col == row) {
+                    currentRow.add(1); // First and last elements are always 1
+                } else {
+                    int value = triangle.get(row - 1).get(col - 1) + triangle.get(row - 1).get(col);
+                    currentRow.add(value);
+                }
+            }
+            triangle.add(currentRow);
+        }
+
+        return triangle;
+    }
+}
